25ab6808b4d16c786dcbe50a07aa00d485e4cc73
Dan Luo <d_luo@umail.ucsb.edu>
2015-04-23 21:27:18 -0700

    Added layout of instagram object

--- app/assets/javascripts/APIClient.js
+++ app/assets/javascripts/APIClient.js
@@ -16,8 +16,9 @@ var APIClient = APIClient || {
 			
 		  },
 	  success: function(data) {
-			tweetList = getTwitterData(data['tweets']); // add if statement later
-	    callback(tweetList);
+		tweetList = getTwitterData(data['tweets']); // add if statement later
+	    instagramList = getInstagramData(data[?]); //to be filled in, and add an if statement
+		callback(tweetList);
 	  },
 	  failure: function(error) {
 	    console.log(error);
@@ -28,7 +29,7 @@ var APIClient = APIClient || {
 
 function getTwitterData(data){
 	//Create an array to hold tweets
-	tweetList = []; 
+	tweetList = [];
 	//Loop through data creating tweets
 	for(var i=0; i<data.length; i++)
 	{
@@ -40,7 +41,6 @@ function getTwitterData(data){
 
 //UI.layout(tweet);
 var Tweet = function(data){
-	console.log(data);
 	this.username = data.user;
 	//this.hashtags = data.hashtags.text;
 	this.text = data.text;
@@ -51,8 +51,23 @@ var Tweet = function(data){
 	//this.profile_image = data.statuses.user.profile_image_url; //url
 }
 
-	
+function getInstagramData(data){
+		//create an array to hold instagram posts
+		instagramList = [];
+		//Loop through data creating grams
+		for(var i=0; i<data.length;i++)
+		{
+				var gram = new Gram(data[i]);
+				instagramList.push(gram)
+		}
+		return instagramList; 
+}	
 
+var Gram = function(data){
+	this.username = data.user.username;
+	this.id = data.caption.from.id;
+	this.text = data.caption.text; 
+}
 
 
 
