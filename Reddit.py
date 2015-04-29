9ff9ceaad06ddbf277cafbfedac11b530555bec1
Vince Gandolfo <vgandolfo@umail.ucsb.edu>
2015-04-29 01:52:38 -0700

    Pushing correct Reddit file

--- Reddit.py
+++ Reddit.py
@@ -1,1 +1,11 @@
-Reddit.py
+"""
+Getting Reddit comments using PRAW
+"""
+import time
+import praw
+
+r = praw.Reddit('Search for specific Reddit comments for VistaSearch')
+length = len(sys.argv)
+comments = r.search(sys.argv[length - 1], 'all', 'new', limit = 10)
+
+return [str(x) for x in comments] 
